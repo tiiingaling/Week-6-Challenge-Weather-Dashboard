dbe111ad92c1c2d90821ef4c1dc8960ec6bd2ad2
tiiingaling <wongwong02@live.co.uk>
2022-11-04 03:32:42 +0000

    added basic files and skeleton code

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//pseudo code
+
+//search input (location/city)
+//5 day current and future weather conditions for that city
+//city added to search history
+//city name. date. icon of weather, temperature, humidity wind speed
+//click on city in history, showed the conditions again
+
+//end
+
