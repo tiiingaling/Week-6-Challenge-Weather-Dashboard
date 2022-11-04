7d2a29f7532768827c6626c26a4c6db53512934a
tiiingaling <wongwong02@live.co.uk>
2022-11-04 12:24:34 +0000

    display current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,28 +44,45 @@ var lookupLocation = (search) => {
 
 var displayWeather = (weatherData) => {
     document.getElementById('location-name').textContent = (weatherData.name), (weatherData.country)
-
-    //getWeather(weatherData.lat, weatherData.lon)
-    console.log(weatherData)
+console.log('displayWeather')
+    getWeather(weatherData.lat, weatherData.lon)
 };
 
 
-//var getWeather = (lat, lon) => {
-
-    //var apiUrl = `${WEATHER_API_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
-    //fetch(apiUrl)
-        //.then(response => response.json())
-        //.then(data => {
-            //displayCurrent(data)
+var getWeather = (lat, lon) => {
+    var apiUrl = `${WEATHER_API_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayCurrent(data)
             //displayForecast(data)
+            console.log('here is data')
+            console.log(data)
             //write functions for current and 5 day forecast
-        //})
-//}
+        })
+}
+
+//uvi
+//temperature
+//wind speed
+//humidity
+//weather icon
+
 
 var displayCurrent = (weatherData) => {
-    var current = weatherData.current;
+    var humidity = weatherData.current.humidity;
+    console.log(humidity)
+    var temp = weatherData.current.temp;
+    console.log(temp)
+    var uvi = weatherData.current.uvi;
+    console.log(uvi)
+    var windSpeed = weatherData.current.wind_speed
+    console.log(windSpeed)
 
-
+    document.getElementById('humid-value').textContent = humidity
+    document.getElementById('temp-value').textContent = temp
+    document.getElementById('uv-value').textContent = uvi
+    document.getElementById('wind-value').textContent = windSpeed
 }
 
 searchButton.addEventListener('click', getLocation);
