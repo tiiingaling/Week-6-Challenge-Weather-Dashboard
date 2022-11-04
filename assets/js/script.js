c52353faf187348f0965ced6f2096f44c85171f3
tiiingaling <wongwong02@live.co.uk>
2022-11-04 04:09:43 +0000

    added API and key, added search button event listener but search function not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,36 @@
 
 //end
 
+
+var WEATHER_API_URL = 'api.openweathermap.org';
+var WEATHER_API_KEY = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var searchButton = document.getElementById('search');
+
+const getLocation = () => {
+    var locationInput = document.getElementById('location')
+    var userLocation = locationInput.value;
+    console.log(userLocation)
+};
+
+var lookupLocation = (search) => {
+
+    var apiURL = `${WEATHER_API_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${WEATHER_API_KEY}`
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(data => {
+
+        var location = data[0];
+        console.log(location);
+
+        //displayWeather(location)
+    })
+
+
+}
+
+
+
+
+
+searchButton.addEventListener('click', getLocation);
+
