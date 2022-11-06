a374f44592500d0d1723a42480620d2992498f65
tiiingaling <wongwong02@live.co.uk>
2022-11-06 03:40:37 +0000

    wrote loop to dynamically create HTML blocks. Not working yet.

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,30 +93,69 @@ var forecastList = document.getElementById('forecast-days')
 
 //creates and displays the blocks for 5 day forecast
 var displayForecast = (weatherData) => {
-
-    var dailyWeather = weatherData.daily[0];
-    
+  
     //make the forecast div show up
     document.getElementById('forecast').style.display= 'block';
 
-    var temp = dailyWeather.temp.day;
-    var windSpeed = dailyWeather.wind_speed;
-    var uvi = dailyWeather.uvi;
-    var humidity = dailyWeather.humidity;
+    //clears html
+    var list = document.getElementById('forecast-days')
+    list.innerHTML = '';
 
-    var UNIX = (dailyWeather.dt * 1000)
-    var options = {weekday: 'long', month: 'long', day: 'numeric'}
-    var datestamp = new Date(UNIX).toLocaleDateString("en-gb", options)
-    console.log(datestamp)
+//creates multiple blocks to display forecast
+    for (let i = 0; i < 4; i++) {
+        var dailyWeather = weatherData.daily[i];
+    
+        //date conversion from UNIX
+        var UNIX = (dailyWeather.dt * 1000)        
+        var options = {weekday: 'long', month: 'long', day: 'numeric'}
 
+        console.log(datestamp)
 
-    document.getElementById('date').textContent = datestamp;
-    document.getElementById('temp-forecast').textContent = temp + ' °F';
-    document.getElementById('wind-forecast').textContent = windSpeed + ' mph';
-    document.getElementById('uv-forecast').textContent = uvi;
-    document.getElementById('humid-forecast').textContent = humidity;
-  
-}
+        var datestamp = new Date(UNIX).toLocaleDateString("en-gb", options)
+        var temp = dailyWeather.temp.day;
+        var windSpeed = dailyWeather.wind_speed;
+        var uvi = dailyWeather.uvi;
+        var humidity = dailyWeather.humidity;
+
+        var date = document.getElementById('date').textContent = datestamp;
+        var tempValue = document.getElementById('temp-forecast').textContent = temp + ' °F';
+        var windValue = document.getElementById('wind-forecast').textContent = windSpeed + ' mph';
+        var humidValue = document.getElementById('humid-forecast').textContent = humidity + ' &';
+        var uvValue = document.getElementById('uv-forecast').textContent = uvi;
+
+        var newDay = document.createElement('div');
+        newDay.classList.add('day');
+        newDay.innerHTML =   
+        `<div class="info">
+            <div class="date">
+                <span>Date:</span>
+                <span id="date">${date}</span>
+            </div>
+
+            <div class="temperature">
+                <span>temp:</span>
+                <span id="temp-forecast">${tempValue}</span>
+            </div>
+
+            <div class="wind">
+                <span>Wind Speed:</span>
+                <span id="wind-forecast">${windValue}</span>
+            </div>
+
+            <div class="humidity">
+                <span>Humidity:</span>
+                <span id="humid-forecast">${humidValue}</span>
+            </div>
+
+            <div class="uv-index">
+                <span>UV Index:</span>
+                <span id="uv-forecast">${uvValue}</span>
+            </div>
+        </div>`;
+    
+        list.appendChild(newDay);
+        }
+  }
 
 //create divs dynamically?
 //document.createElement('div')
